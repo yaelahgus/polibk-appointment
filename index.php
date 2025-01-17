2965c16f06491b5bf7c49936608861536f26f99c
Yaelahgus <Ramikomichelvelez@gmail.com>
2025-01-18 06:41:45 +0700

    Update terbaru boskuuuuu

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Poliklinik Dian Nuswantoro - PoliDinus</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="public/Eterna/assets/img/favicon.png" rel="icon">
+  <link href="public/Eterna/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="public/Eterna/assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="public/Eterna/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="public/Eterna/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="public/Eterna/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="public/Eterna/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="public/Eterna/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+
+  <!-- Template Main CSS File -->
+  <link href="public/Eterna/assets/css/style.css" rel="stylesheet">
+
+
+  <!-- =======================================================
+  * Template Name: Eterna
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.html"><img src="public/Eterna/assets/img/apple-touch-icon.png" alt="" class="img-fluid"> PoliDinus</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="#">Home</a></li>
+            
+            <li><a class="active" href="#about">About</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url('public/Eterna/assets/img/slide/bg_poli2.jpg')">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Welcome to <span>PoliDinus</span></h2>
+                <p class="animate__animated animate__fadeInUp">Sistem temu janji berbasis web yang dirancang untuk mendukung layanan kesehatan di Poliklinik Dian Nuswantoro, merupakan fasilitas kesehatan warga Universitas Dian Nuswantoro (Udinus).</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item active" style="background-image: url('public/Eterna/assets/img/slide/bg_poli1.png')">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated fanimate__adeInDown">PoliDinus: <span> Temu Janji, Solusi Kesehatan Anda</span></h2>
+                <p class="animate__animated animate__fadeInUp">Sistem temu janji berbasis web yang dirancang untuk mendukung layanan kesehatan di Poliklinik Dian Nuswantoro, merupakan fasilitas kesehatan warga Universitas Dian Nuswantoro (Udinus).</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Featured Section ======= -->
+    <section id="featured" class="featured">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6 mt-6 mt-lg-0">
+            <div class="icon-box">
+              <i class="bi bi-person-wheelchair"></i>
+              <h3><a href="routes/login/loginUser.php">Login Pasien</a></h3>
+              <p>Login sebagai pasien Dinusian PoliDinus</p>
+            </div>
+          </div>
+          <div class="col-lg-6 mt-6 mt-lg-0">
+            <div class="icon-box">
+              <i class="bi bi-heart-pulse-fill"></i>
+              <h3><a href="routes/login/login.php">Login Dokter</a></h3>
+              <p>Login sebagai dokter Dinusian PoliDinus</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Featured Section -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+         
+          <div class="col-lg-12 pt-4 pt-lg-0 content ">
+            <h2>Tentang Kami,</h2>
+            <p class="fst-italic">
+              
+          PoliDinus adalah sistem temu janji berbasis web yang dirancang untuk meningkatkan kemudahan dan efisiensi layanan kesehatan di Poliklinik Dian Nuswantoro. Sebagai fasilitas kesehatan resmi untuk warga Universitas Dian Nuswantoro (Udinus), PoliDinus hadir untuk menjembatani kebutuhan pasien dalam mengatur janji temu dengan dokter secara cepat dan praktis.
+
+          Sistem ini dirancang dengan teknologi modern untuk memastikan setiap pasien mendapatkan pelayanan kesehatan yang terpercaya, efisien, dan transparan. PoliDinus merupakan solusi inovatif yang mendukung upaya digitalisasi layanan kesehatan, memberikan akses mudah bagi seluruh sivitas akademika Udinus dan masyarakat sekitarnya.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Pelayanan Terpercaya dan Efisien</li>
+              <li><i class="bi bi-check-circle"></i> Akses Mudah dan Cepat</li>
+              <li><i class="bi bi-check-circle"></i> Pelayanan Transparan dan Ramah</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </main>
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>PoliDinus</span></strong>. All Rights Reserved 2024.
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+<script src="public/Eterna/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="public/Eterna/assets/vendor/aos/aos.js"></script>
+<script src="public/Eterna/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="public/Eterna/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="public/Eterna/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="public/Eterna/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="public/Eterna/assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="public/Eterna/assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="public/Eterna/assets/js/main.js"></script>
+
+
+</body>
+
+</html>
+
+<!-- /* 
+Bimbingan Karir
+
+Agus Arifudin
+A11.2021.13736
+Web Developer - 05
+*/ -->
